c236a92c8357623c19cce0c4dd9a2e9866a29b51
Retryelle <retryelle@gmail.com>
2025-07-18 10:39:14 +0300

    Create programs/zsh.nix system module

---
+++ hosts/LatitudeE5570/modules/programs/zsh.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+{
+  programs.zsh = {
+    enable = true;
+    package = pkgs.zsh;
+    
+    histSize = 10000;
+    histFile = "$HOME/.zsh_history";
+
+    enableLsColors = true;
+    enableCompletion = true;
+    
+    shellInit = "";
+
+    promptInit = ''
+      prompt off
+      eval "$(starship init zsh)"
+    '';
+
+    autosuggestions = {
+      enable = true;
+      async = true;
+      strategy = [ "history" "completion" ];
+    };
+
+    syntaxHighlighting = {
+      enable = true;
+      highlighters = [ "main" "brackets" "cursor" "root" "line" ];
+    };
+
+    ohMyZsh = {
+      enable = true;
+      package = pkgs.oh-my-zsh;
+      
+      plugins = [
+        "colored-man-pages"
+        "extract"
+        "fzf"
+        "git"
+        "sudo"
+        "z"
+      ];
+    };
+  };
+}
